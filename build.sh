c073a6eb99f17e2f6772d2210606c686683b8ab6
victor Santos Uceta <victor.uceta@gmail.com>
2017-03-02 14:37:14 -0500

    adding correct build folder

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # compile
 mvn clean compile assembly:single
 # compress
-tar -zcvf ./dist/trueno-elastic-bridge-server.tar.gz ./target/trueno-elastic-bridge-server.jar
+tar -zcvf ./build/trueno-elastic-bridge-server.tar.gz ./target/trueno-elastic-bridge-server.jar ./bridgeServer.sh
