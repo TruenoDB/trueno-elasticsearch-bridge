07f5035c2ef0bd90cb6d7a9e1c6c00b6bb037241
victor Santos Uceta <victor.uceta@gmail.com>
2017-03-02 15:15:51 -0500

    - adding new build

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@
 
 # compile
 mvn clean compile assembly:single
+#copy to a temporary directory
+rm -rf ./build/*
+mkdir ./build/server
+cp ./target/trueno-elastic-bridge-server.jar ./build/server/
+cp ./bridgeServer.sh ./build/server/
+cd ./build
 # compress
-tar -zcvf ./build/trueno-elastic-bridge-server.tar.gz ./target/trueno-elastic-bridge-server.jar ./bridgeServer.sh
+tar -zcvf trueno-elastic-bridge-server.tar.gz ./server
+cd ..
+rm -rf ./build/server
